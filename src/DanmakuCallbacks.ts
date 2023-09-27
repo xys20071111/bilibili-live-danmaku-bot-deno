93a56ad95360874a32d0250a08a48ae8daff5a38
xys20071111 <xys20071111@hotmail.com>
2023-09-27 19:57:30 +0800

    现在可以只用房间号来初始房间监听了

--- src/DanmakuCallbacks.ts
+++ src/DanmakuCallbacks.ts
@@ -1,6 +1,6 @@
 // deno-lint-ignore-file
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import config from './Config.ts'
+import config, { Credential } from './Config.ts'
 import { RoomConfig } from './Config.ts'
 import { sendDanmaku } from './SendDanmaku.ts'
 import { Encoding } from './Text.ts'
@@ -13,54 +13,118 @@ let logFile = Deno.openSync(`./log/${getTimeString()}.log`, {
 const thanksColdDownSet = new Set<string>()
 const { defaultConfig } = config
 
-export function buildCallbackByRoomConfig(room: RoomConfig) {
-  const verify = room.verify || config.verify
-  const receiveGift = (roomId: number, data: any) => {
-    if (thanksColdDownSet.has(data.uname)) {
-      return
+function isRoomConfig(room: RoomConfig | number): room is RoomConfig {
+  return typeof (room as RoomConfig).room_id !== 'undefined'
+}
+
+export function buildCallbackByRoomConfig(room: RoomConfig | number) {
+
+  if (isRoomConfig(room) && room.verify) {
+    const verify = room.verify
+    const receiveGift = (roomId: number, data: any) => {
+      if (thanksColdDownSet.has(data.uname)) {
+        return
+      }
+      logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${data.uname} 投喂了${data.super_gift_num}个 ${data.giftName} 价值${data.price / 1000 * data.super_gift_num}元\n`))
+      if (room.free_gift_action || data.super_gift_num > 0) {
+        sendDanmaku(roomId, {
+          msg: FormatString((room.danmakus?.gift || defaultConfig.danmakus.gift), { name: data.uname, gift: data.giftName })
+        }, verify)
+        thanksColdDownSet.add(data.uname)
+        setTimeout(() => { thanksColdDownSet.delete(data.uname) }, room.cold_down_time)
+      }
     }
-    logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${data.uname} 投喂了${data.super_gift_num}个 ${data.giftName} 价值${data.price / 1000 * data.super_gift_num}元\n`))
-    if (room.free_gift_action || data.super_gift_num > 0) {
+    const onTotalGift = (roomId: number, data: any) => {
+      logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${data.uname}投喂了${data.total_num}个${data.gift_name}\n`))
       sendDanmaku(roomId, {
-        msg: FormatString((room.danmakus?.gift || defaultConfig.danmakus.gift), { name: data.uname, gift: data.giftName })
+        msg: FormatString((room.danmakus?.gift_total || defaultConfig.danmakus.gift_total), { name: data.uname, gift: data.gift_name, count: data.total_num })
       }, verify)
-      thanksColdDownSet.add(data.uname)
-      setTimeout(() => { thanksColdDownSet.delete(data.uname) }, room.cold_down_time)
     }
-  }
-  const onTotalGift = (roomId: number, data: any) => {
-    logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${data.uname}投喂了${data.total_num}个${data.gift_name}\n`))
-    sendDanmaku(roomId, {
-      msg: FormatString((room.danmakus?.gift_total || defaultConfig.danmakus.gift_total), { name: data.uname, gift: data.gift_name, count: data.total_num })
-    }, verify)
-  }
-  const receiveDanmaku = (roomId: number, data: any) => {
-    logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${roomId} ${data[2][1]}:${data[2][0]}  ${data[1]}\n`))
-  }
-  const onLiveStart = (roomId: number) => {
-    if (skipCount != 1) {
-      skipCount++
-      return
+    const receiveDanmaku = (roomId: number, data: any) => {
+      logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${roomId} ${data[2][1]}:${data[2][0]}  ${data[1]}\n`))
     }
-    skipCount = 0
-    sendDanmaku(roomId, { msg: (room.danmakus?.live_start || defaultConfig.danmakus.live_start) }, verify)
-    logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${roomId}直播开始\n`))
+    const onLiveStart = (roomId: number) => {
+      if (skipCount != 1) {
+        skipCount++
+        return
+      }
+      skipCount = 0
+      sendDanmaku(roomId, { msg: (room.danmakus?.live_start || defaultConfig.danmakus.live_start) }, verify)
+      logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${roomId}直播开始\n`))
+    }
+    const onLiveEnd = (roomId: number) => {
+      logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${roomId}直播结束\n`))
+      sendDanmaku(roomId, { msg: (room.danmakus?.live_end || defaultConfig.danmakus.live_end) }, verify)
+    }
+    const onGraud = (roomId: number, data: any) => {
+      logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${data.username}:${data.uid} 购买了 ${data.gift_name}\n`))
+      sendDanmaku(roomId, {
+        msg: FormatString((room.danmakus?.guard || defaultConfig.danmakus.guard), { type: data.gift_name, name: data.username })
+      }, verify)
+    }
+    const onSuperChat = (roomId: number, data: any) => {
+      logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${data.user_info.uname}发送了SC 价格${data.price}\n`))
+      sendDanmaku(roomId, {
+        msg: FormatString((room.danmakus?.sc || defaultConfig.danmakus.sc), { name: data.user_info.uname })
+      }, verify)
+    }
+    return { receiveGift, onTotalGift, receiveDanmaku, onLiveStart, onLiveEnd, onGraud, onSuperChat }
+  } else {
+    const verify = config.verify
+    const receiveGift = (roomId: number, data: any) => {
+      if (thanksColdDownSet.has(data.uname)) {
+        return
+      }
+      logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${data.uname} 投喂了${data.super_gift_num}个 ${data.giftName} 价值${data.price / 1000 * data.super_gift_num}元\n`))
+      if (defaultConfig.free_gift_action || data.super_gift_num > 0) {
+        sendDanmaku(roomId, {
+          msg: FormatString(defaultConfig.danmakus.gift, {
+            name: data.uname,
+            gift: data.giftName
+          })
+        }, verify)
+        thanksColdDownSet.add(data.uname)
+        setTimeout(() => { thanksColdDownSet.delete(data.uname) }, defaultConfig.cold_down_time)
+      }
+    }
+    const onTotalGift = (roomId: number, data: any) => {
+      logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${data.uname}投喂了${data.total_num}个${data.gift_name}\n`))
+      sendDanmaku(roomId, {
+        msg: FormatString(defaultConfig.danmakus.gift_total, {
+          name: data.uname,
+          gift: data.gift_name, count: data.total_num
+        })
+      }, verify)
+    }
+    const receiveDanmaku = (roomId: number, data: any) => {
+      logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${roomId} ${data[2][1]}:${data[2][0]}  ${data[1]}\n`))
+    }
+    const onLiveStart = (roomId: number) => {
+      if (skipCount != 1) {
+        skipCount++
+        return
+      }
+      skipCount = 0
+      sendDanmaku(roomId, { msg: defaultConfig.danmakus.live_start }, verify)
+      logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${roomId}直播开始\n`))
+    }
+    const onLiveEnd = (roomId: number) => {
+      logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${roomId}直播结束\n`))
+      sendDanmaku(roomId, { msg: defaultConfig.danmakus.live_end }, verify)
+    }
+    const onGraud = (roomId: number, data: any) => {
+      logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${data.username}:${data.uid} 购买了 ${data.gift_name}\n`))
+      sendDanmaku(roomId, {
+        msg: FormatString(defaultConfig.danmakus.guard, { type: data.gift_name, name: data.username })
+      }, verify)
+    }
+    const onSuperChat = (roomId: number, data: any) => {
+      logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${data.user_info.uname}发送了SC 价格${data.price}\n`))
+      sendDanmaku(roomId, {
+        msg: FormatString(defaultConfig.danmakus.sc, { name: data.user_info.uname })
+      }, verify)
+    }
+    return { receiveGift, onTotalGift, receiveDanmaku, onLiveStart, onLiveEnd, onGraud, onSuperChat }
   }
-  const onLiveEnd = (roomId: number) => {
-    logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${roomId}直播结束\n`))
-    sendDanmaku(roomId, { msg: (room.danmakus?.live_end || defaultConfig.danmakus.live_end) }, verify)
-  }
-  const onGraud = (roomId: number, data: any) => {
-    logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${data.username}:${data.uid} 购买了 ${data.gift_name}\n`))
-    sendDanmaku(roomId, {
-      msg: FormatString((room.danmakus?.guard || defaultConfig.danmakus.guard), { type: data.gift_name, name: data.username })
-    }, verify)
-  }
-  const onSuperChat = (roomId: number, data: any) => {
-    logFile.writeSync(Encoding.UTF8.getBytes(`${getTimeString()} ${data.user_info.uname}发送了SC 价格${data.price}\n`))
-    sendDanmaku(roomId, {
-      msg: FormatString((room.danmakus?.sc || defaultConfig.danmakus.sc), { name: data.user_info.uname })
-    }, verify)
-  }
-  return { receiveGift, onTotalGift, receiveDanmaku, onLiveStart, onLiveEnd, onGraud, onSuperChat }
+
 }
--- src/Main.ts
+++ src/Main.ts
@@ -10,7 +10,38 @@ await launchAllPlugins()
 
 for (const room of config.rooms) {
   if (typeof room === 'number') {
-
+    if (room === 0) {
+      continue
+    }
+    const danmakuReceiver = new DanmakuReceiver(room, config.verify)
+    danmakuReceiver.on('connected', () => {
+      printLog('主程序', `[${room}]连接成功`)
+    })
+    const { onGraud, onSuperChat, onTotalGift, onLiveStart, onLiveEnd, receiveDanmaku, receiveGift } = buildCallbackByRoomConfig(room)
+    if (!config.defaultConfig.disable_gift_action) {
+      danmakuReceiver.on('COMBO_SEND', onTotalGift)
+      danmakuReceiver.on('SEND_GIFT', receiveGift)
+    }
+    if (!config.defaultConfig.disable_super_chat_action) {
+      danmakuReceiver.on('GUARD_BUY', onGraud)
+    }
+    if (!config.defaultConfig.disable_super_chat_action) {
+      danmakuReceiver.on('SUPER_CHAT_MESSAGE', onSuperChat)
+    }
+    globalThis.onunload = () => {
+      printLog('主程序', '退出')
+    }
+    danmakuReceiver.on('closed', (reason: string) => {
+      printLog('主程序', `[${room}]掉线了 ${reason}`)
+      danmakuReceiver.connect().then()
+    })
+    if (!config.defaultConfig.disable_greeting) {
+      danmakuReceiver.on('LIVE', onLiveStart)
+      danmakuReceiver.on('PREPARING', onLiveEnd)
+    }
+    danmakuReceiver.on('DANMU_MSG', receiveDanmaku)
+    await danmakuReceiver.connect()
+    roomReceiverMap.set(room, danmakuReceiver)
   } else {
     if (room.room_id === 0) {
       continue
